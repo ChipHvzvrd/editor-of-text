cf30a34db6bab1d2a3507465eb9ca446d034929e
chiphvzvrd <christianipena@yahoo.com>
2022-11-05 00:59:50 -0500

    generate SW

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -21,9 +24,21 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
-      })
-    ],
-
+      }),
+      new WorkboxPlugin.GenerateSW({
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+    runtimeCaching: [{
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 1,
+        },
+      },
+    }]
+  })],
+    
     module: {
       rules: [
         {
